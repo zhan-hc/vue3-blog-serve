f18ddcdaf6264543590a3f22ef889354be5a196c
zhan-hc <1497903789@qq.com>
2021-11-22 01:17:01 +0800

    feat: 增加文件分片上传合并接口

--- routes/blogSystem/upload.js
+++ routes/blogSystem/upload.js
@@ -1,11 +1,13 @@
+const fs = require('fs')
+const path = require('path')
 const router = require('koa-router')()
 const multer = require('koa-multer')
 router.prefix('/blog/upload')
+const operate = require('../../utils/common')
 // 上传 图片
-var storage = multer.diskStorage({
-  //文件保存路径
-  destination: function(req, file, cb) {
-  cb(null, 'public/images/')
+var storage = (path="images") => multer.diskStorage({
+  destination: function(ctx, file, cb) {
+  cb(null, `public/${path}/`)
   },
   //修改文件名称
   filename: function(req, file, cb) {
@@ -14,15 +16,54 @@ var storage = multer.diskStorage({
   }
 })
 //加载配置
-var upload = multer({
-storage: storage
+var upload = (path) => multer({
+storage: storage(path)
 });
-// upload.single('file'),
-router.post('/images', upload.single('file'), async(ctx, next) => {
+
+
+router.post('/images', upload().single('file'), async(ctx, next) => {
   ctx.success(
     {
       filename: ctx.req.file.filename //返回文件名
     }
     ,'上传文件成功')
 })
+
+
+router.post('/file', upload('file').single('file'), async(ctx, next) => {
+  const folderName = ctx.req.body.fileName
+  const fileName = ctx.req.file.filename
+  const chunkName = `${ctx.req.body.chunkName}.${ctx.req.file.originalname}`
+  try {
+      const filePath = path.join(__dirname,`../../public/file/${folderName}`)
+      await operate.exitsFolder(filePath);
+      setTimeout(() => {
+        operate.moveFile(fileName,folderName,chunkName)
+      },500)
+      
+  } catch (e) {
+      throw Error(e.msg);
+  }
+  ctx.success(fileName,'上传文件成功')
+})
+
+router.post('/mergeChunkFile', async(ctx, next) => {
+  const {fileName, chunkName} = ctx.request.body
+  // 存放切块的路径
+  const chunkDir = path.join(__dirname,`../../public/file/${fileName}`)
+  
+  await operate.mergeFileChunk(chunkDir,chunkName)
+  fs.rmdirSync(path.join(__dirname,`../../public/file/${chunkName}`));
+ 
+  ctx.success('buf','上传文件成功')
+})
+
+router.post('/verifyFile', async(ctx, next) => {
+  const {key} = ctx.request.body
+  const shouldUpload = operate.hasFile(key)
+  ctx.success({
+    shouldUpload
+  },'上传文件成功')
+})
+
 module.exports = router
---
+++ utils/common.js
@@ -0,0 +1,102 @@
+const fs = require('fs')
+const path = require('path')
+const crypto = require('crypto');
+
+// 判断文件夹是否存在
+exitsFolder = async function (reaPath) {
+  const absPath = path.resolve(__dirname, reaPath);
+  fs.stat(absPath, function (err, stats) {
+      if (!stats) {
+          fs.mkdir(absPath, {recursive: true}, err => {
+              if (err) throw err;
+          }); //Create dir in case not found
+      }
+  });
+}
+// 移动文件
+moveFile = function (fileName,movePath,newFileName) {
+  var sourceFile = path.join(__dirname, `../public/file/${fileName}`);
+  var destPath = path.join(__dirname, `../public/file/${movePath}`, newFileName);
+  console.log(sourceFile,'---',destPath)
+fs.rename(sourceFile, destPath, function (err) {
+  if (err) throw err;
+  fs.stat(destPath, function (err, stats) {
+    if (err) throw err;
+    // console.log('stats: ' + JSON.stringify(stats));
+  });
+});
+}
+
+const pipeStream = (path, writeStream) => {
+  console.log('path', path)
+  return new Promise(resolve => {
+      const readStream = fs.createReadStream(path);
+      readStream.on("end", () => {
+          fs.unlinkSync(path);
+          resolve();
+      });
+      readStream.pipe(writeStream);
+  });
+}
+
+// 合并切片
+const mergeFileChunk = async (filePath, chunkName, size = 5 * 1024 * 1024) => {
+  
+  // filePath：将切片合并的路径
+  // 切片文件路径
+  const chunkDir = path.join(__dirname,`../public/file/${chunkName}`)
+  let chunkPaths = null
+  // 获取切片文件夹里所有切片，返回一个数组
+  chunkPaths = fs.readdirSync(`public/file/${chunkName}`)
+  // 根据切片下标进行排序
+  // 否则直接读取目录的获得的顺序可能会错乱
+  // 截取索引
+  const eq = (str) => parseInt(str.slice(str.lastIndexOf('-')+1,str.lastIndexOf('.')))
+  // 因为是异步请求所以索引顺序不一定一致
+  chunkPaths.sort((a, b) => eq(a) - eq(b))
+  console.log(`public/file/${chunkName}`, chunkPaths,'chunkPaths')
+  const arr = chunkPaths.map((chunkPath, index) => {
+      return pipeStream(
+          path.resolve(chunkDir, chunkPath),
+          // 指定位置创建可写流
+          fs.createWriteStream(filePath, {
+              start: index * size,
+              end: (index + 1) * size
+          })
+      )
+  })
+  await Promise.all(arr)
+};
+
+// 获取文件hash
+function createHash (fileName) {
+  const buffer = fs.readFileSync(path.join(__dirname,`../public/file/${fileName}`));
+  const fsHash = crypto.createHash('md5');
+
+  fsHash.update(buffer);
+  const md5 = fsHash.digest('hex');
+  return md5
+}
+
+// 文件hash校验
+function hasFile (fileHash) {
+  filePaths = fs.readdirSync(`public/file`)
+  const len = filePaths.length
+  let flag = true
+  for(let i = 0;i<len;i++) {
+    const hash = createHash(filePaths[i])
+    if (fileHash === hash){
+      flag = false
+      break
+    }
+  }
+  console.log(flag,'-------flag')
+  return flag
+}
+
+module.exports= {
+  moveFile,
+  hasFile,
+  exitsFolder,
+  mergeFileChunk
+}
